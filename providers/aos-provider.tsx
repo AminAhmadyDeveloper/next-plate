4bc5fa31fd203c4370dd4d078baa309b10a53c90
Amin Ahmady <aminahmadydeveloper@gmail.com>
2024-09-30 12:53:22 +0330

    ci: init project with nextjs

--- providers/aos-provider.tsx
+++ providers/aos-provider.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { init } from 'aos';
+import AOS from 'aos';
 
 import type { FC } from 'react';
 import { useEffect } from 'react';
 
 export const AosProvider: FC = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(init, []);
+  useEffect(AOS.init, []);
   return null;
 };
