5d0beefb4f466928ab38edf2e0f7355f36f82b37
Amin Ahmady <aminahmadydeveloper@gmail.com>
2024-10-03 09:39:21 +0330

    feat: upstash ratelimit added to trpc as middleware

--- lib/trpc-utils.ts
+++ lib/trpc-utils.ts
@@ -151,11 +151,11 @@ export const defineLimiterWithProps = <
   };
 };
 
-export const defaultFingerPrint = (req: Request | Record<any, any>) => {
+export const defaultFingerPrint = (req?: Request | Record<any, any>) => {
   const forwarded =
     req instanceof Request
       ? req.headers.get('x-forwarded-for')
-      : req.headers['x-forwarded-for'];
+      : req?.headers['x-forwarded-for'];
   const ip = forwarded
     ? (typeof forwarded === 'string' ? forwarded : forwarded[0])?.split(/, /)[0]
     : ((req as any)?.socket?.remoteAddress ?? null);
--- trpc/server.ts
+++ trpc/server.ts
@@ -1,29 +1,29 @@
 import { initTRPC } from '@trpc/server';
 import type { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
-// import { Ratelimit } from '@upstash/ratelimit';
+import { Ratelimit } from '@upstash/ratelimit';
 import Superjson from 'superjson';
 
-// import {
-//   createTRPCUpstashLimiter,
-//   defaultFingerPrint,
-//   redis,
-// } from '@/lib/upstash-utils';
+import {
+  createTRPCUpstashLimiter,
+  defaultFingerPrint,
+  redis,
+} from '@/lib/upstash-utils';
 
-// const rateLimiter = createTRPCUpstashLimiter<typeof t>({
-//   fingerprint: (ctx) => defaultFingerPrint(ctx.req),
-//   message: (hitInfo) =>
-//     `Too many requests, please try again later. ${Math.ceil(
-//       (hitInfo.reset - Date.now()) / 1000,
-//     )}`,
-//   max: 15,
-//   windowMs: 10000,
-//   rateLimitOpts(opts) {
-//     return {
-//       redis: redis,
-//       limiter: Ratelimit.fixedWindow(opts.max, `${opts.windowMs} ms`),
-//     };
-//   },
-// });
+const rateLimiter = createTRPCUpstashLimiter<typeof t>({
+  fingerprint: (ctx) => defaultFingerPrint(ctx.request),
+  message: (hitInfo) =>
+    `Too many requests, please try again later. ${Math.ceil(
+      (hitInfo.reset - Date.now()) / 1000,
+    )}`,
+  max: 1,
+  windowMs: 10000,
+  rateLimitOpts(opts) {
+    return {
+      redis: redis,
+      limiter: Ratelimit.fixedWindow(opts.max, `${opts.windowMs} ms`),
+    };
+  },
+});
 
 export const createTRPCContext = (options?: FetchCreateContextFnOptions) => {
   return {
@@ -35,5 +35,5 @@ const t = initTRPC
   .create({ transformer: Superjson });
 
 export const router = t.router;
-export const publicProcedure = t.procedure;
+export const publicProcedure = t.procedure.use(rateLimiter);
 export const createCallerFactory = t.createCallerFactory;
